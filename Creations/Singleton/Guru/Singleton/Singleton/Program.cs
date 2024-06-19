25291d80764dbb5eace8ac1d35bb22e7fbb268b3
Kangulungu <kangulungu@gmail.com>
2024-06-19 13:30:12 +0200

    Singleton Thread Safe

--- Creations/Singleton/Guru/Singleton/Singleton/Program.cs
+++ Creations/Singleton/Guru/Singleton/Singleton/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace Singleton
@@ -10,19 +11,49 @@ namespace Singleton
     {
         static void Main(string[] args)
         {
+            ////Singleton_Naif
+            //Singleton_Naif s1 = Singleton_Naif.GetInstance();
+            //Singleton_Naif s2 = Singleton_Naif.GetInstance();
 
-            Singleton_Naif s1 = Singleton_Naif.GetInstance();
-            Singleton_Naif s2 = Singleton_Naif.GetInstance();
+            //if (s1 == s2)
+            //{
+            //    Console.WriteLine("Singleton works, both variables contain the same instance.");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("Singleton failed, variables contain different instances.");
+            //}
 
-            if (s1 == s2)
+            ////ingleton_TSafe
+            
+            Console.WriteLine(
+                "{0}\n{1}\n\n{2}\n",
+                "If you see the same value, then singleton was reused (yay!)",
+                "If you see different values, then 2 singletons were created (booo!!)",
+                "RESULT:"
+            );
+
+            Thread process1 = new Thread(() =>
             {
-                Console.WriteLine("Singleton works, both variables contain the same instance.");
-            }
-            else
+                TestSingleton("FOO");
+            });
+            Thread process2 = new Thread(() =>
             {
-                Console.WriteLine("Singleton failed, variables contain different instances.");
-            }
+                TestSingleton("BAR");
+            });
+
+            process1.Start();
+            process2.Start();
+
+            process1.Join();
+            process2.Join();
             Console.ReadLine();
+        }
+
+        public static void TestSingleton(string value)
+        {
+            Singleton_TSafe singleton = Singleton_TSafe.GetInstance(value);
+            Console.WriteLine(singleton.Value);
         }
     }
 }
