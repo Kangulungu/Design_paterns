2009f09858ecb120ebf9c5b95a8f3d63360d09e4
Kangulungu <kangulungu@gmail.com>
2024-06-19 13:16:29 +0200

    Singleton Naif

--- Creations/Singleton/Guru/Singleton/Singleton/Program.cs
+++ Creations/Singleton/Guru/Singleton/Singleton/Program.cs
@@ -10,6 +10,19 @@ namespace Singleton
     {
         static void Main(string[] args)
         {
+
+            Singleton_Naif s1 = Singleton_Naif.GetInstance();
+            Singleton_Naif s2 = Singleton_Naif.GetInstance();
+
+            if (s1 == s2)
+            {
+                Console.WriteLine("Singleton works, both variables contain the same instance.");
+            }
+            else
+            {
+                Console.WriteLine("Singleton failed, variables contain different instances.");
+            }
+            Console.ReadLine();
         }
     }
 }
---
+++ Creations/Singleton/Guru/Singleton/Singleton/Singleton_Naif.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Singleton
+{
+    class Singleton_Naif
+    {
+        private static Singleton_Naif _instance;
+
+        public static Singleton_Naif GetInstance()
+        {
+            if (_instance == null)
+            {
+                _instance = new Singleton_Naif();
+            }
+            return _instance;
+        }
+        public void someBusinessLogic()
+        {
+            // ...
+        }
+    }
+}
