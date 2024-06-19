44b2ef7143b76cab104c793ff3dbbc8a766be40b
Kangulungu <kangulungu@gmail.com>
2024-06-19 10:32:26 +0200

    Fichiers Fabrique et Singleton

---
+++ Creations/Fabrique/ConcreteCreator1.cs
@@ -0,0 +1,7 @@
+public class ConcreteCreator1 : Creator
+{
+    public override IProduct FactoryMethod()
+    {
+        return new ConcreteProduct1();
+    }
+}
---
+++ Creations/Fabrique/ConcreteCreator2.cs
@@ -0,0 +1,7 @@
+ public class ConcreteCreator2 : Creator
+    {
+        public override IProduct FactoryMethod()
+        {
+            return new ConcreteProduct2();
+        }
+    }
