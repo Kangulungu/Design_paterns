f7f79cc573d57378cb9d5c950ca7c4f6effc5788
Kangulungu Lubilanji Pite <40600239+Kangulungu@users.noreply.github.com>
2024-06-19 11:51:37 +0200

    Delete Creations directory

--- Creations/Fabrique/ConcreteCreator2.cs
+++
@@ -1,7 +0,0 @@
- public class ConcreteCreator2 : Creator
-    {
-        public override IProduct FactoryMethod()
-        {
-            return new ConcreteProduct2();
-        }
-    }
--- Creations/Fabrique/ConcreteProduct2.cs
+++
@@ -1,7 +0,0 @@
-  class ConcreteProduct2 : IProduct
-    {
-        public string Operation()
-        {
-            return "{Result of ConcreteProduct2}";
-        }
-    }
