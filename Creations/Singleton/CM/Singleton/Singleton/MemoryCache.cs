91d20a1dcbed24da45cb9a49f32b9f0fd2c20ff9
Kangulungu <kangulungu@gmail.com>
2024-06-19 14:36:32 +0200

    Cache memoire; Singleton avec la fonction Lazy + Dictionnaire

---
+++ Creations/Singleton/CM/Singleton/Singleton/MemoryCache.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Singleton
+{
+    public sealed class MemoryCache
+    {
+        private Dictionary<string, object> _cacheObject;
+        private static Lazy<MemoryCache> _cache = new Lazy<MemoryCache>(() => new MemoryCache());
+        public static MemoryCache Instance {
+            get {
+                return _cache.Value;
+            }
+        }
+        private MemoryCache()
+        {
+            _cacheObject = new Dictionary<string, object>();
+        }
+
+        public void Add(string key, object value)
+        {
+            _cacheObject.Add(key, value);
+        }
+        public object Get(string key)
+        {
+            if(_cacheObject.ContainsKey(key))
+             return _cacheObject[key];
+            
+            return null;
+        }
+    }
+}
--- Creations/Singleton/CM/Singleton/Singleton/Program.cs
+++ Creations/Singleton/CM/Singleton/Singleton/Program.cs
@@ -1,2 +1,11 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using Singleton;
+
+var cache =  MemoryCache.Instance;
+var cache2 =  MemoryCache.Instance;
+
+cache.Add("DHeure", DateTime.Now);
+
+Console.WriteLine(cache2.Get("DHeure"));
+
+Console.WriteLine( object.ReferenceEquals(cache, cache2));
